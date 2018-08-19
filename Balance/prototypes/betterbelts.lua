0f36c61d5041021a5c8366985aa658b2d480ab46
Victor <thalassicus@live.com>
2018-08-19 15:18:09 -0500

    Belt speed increase

--- Balance/prototypes/betterbelts.lua
+++ Balance/prototypes/betterbelts.lua
@@ -2,4 +2,6 @@ data.raw["underground-belt"]["underground-belt"].max_distance=20
 data.raw["underground-belt"]["fast-underground-belt"].max_distance=20
 data.raw["underground-belt"]["express-underground-belt"].max_distance=20
 data.raw["underground-belt"]["express-underground-belt"].speed=0.125
+data.raw["transport-belt"]["express-transport-belt"].speed=0.125
+data.raw["splitter"]["express-splitter"].speed=0.125
 data.raw["pipe-to-ground"]["pipe-to-ground"].fluid_box.pipe_connections[2].max_underground_distance=20
